16ab05ce919dc19012ccec0ad9559cde924c1088
fordat <fordatwater@gmail.com>
2024-05-07 18:58:39 -0700

    create simple div and add background

--- main.js
+++ main.js
@@ -10,41 +10,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 let camera, scene, renderer;
 let controls, water, sun;
 
-const loader = new GLTFLoader();
-
-class Boat {
-  constructor() {
-    loader.load("assets/boat5/scene.gltf", (gltf) => {
-      scene.add(gltf.scene);
-      gltf.scene.scale.set(5,5,5);
-      gltf.scene.position.set(0,4,0);
-      gltf.scene.rotation.y = 1.5;
-
-      this.boat = gltf.scene;
-      this.speed = {
-        vel: 0,
-        rot: 0
-      }
-
-      // camera.position.set(0,15,105);
-      // this.boat.add(camera);
-    });
-  }
-
-  update() {
-    if (this.boat) {
-      this.boat.rotation.y += this.speed.rot;
-      this.boat.translateX(this.speed.vel);
-    }
-  }
-
-  // stop() {
-  //   this.speed.rot = 0;
-  //   this.speed.vel = 0;
-  // }
-}
-
-const boat = new Boat();
 
 init();
 animate();
@@ -67,7 +32,6 @@ function init() {
   camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 20000 );
   camera.position.set( -10, 30, 100 );
 
-  camera.add(boat);
 
   // boat.add(camera);
 
@@ -146,43 +110,9 @@ function init() {
 
   updateSun();
 
-  //
-
-  controls = new OrbitControls( camera, renderer.domElement );
-  controls.maxPolarAngle = Math.PI * 0.495;
-  controls.target.set( 0, 10, 0 );
-  controls.minDistance = 40.0;
-  controls.maxDistance = 200.0;
-  controls.update();
-
   const waterUniforms = water.material.uniforms;
 
   window.addEventListener( 'resize', onWindowResize );
-
-  window.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowUp") {
-      boat.speed.vel = 0.7
-    }
-    if (e.key === "ArrowDown") {
-      boat.speed.vel = -0.7
-    }
-    if (e.key === "ArrowRight") {
-      boat.speed.rot = -0.05
-    }
-    if (e.key === "ArrowLeft") {
-      boat.speed.rot = 0.05
-    }
-  });
-
-  window.addEventListener('keyup', function(e) {
-    if (e.key === "ArrowDown" || e.key === "ArrowUp") {
-      boat.speed.vel = 0;
-    }
-
-    if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-      boat.speed.rot = 0;
-    }
-  })
 
 }
 
@@ -196,11 +126,8 @@ function onWindowResize() {
 }
 
 function animate() {
-
-  boat.update();
   requestAnimationFrame( animate );
   render();
-
 }
 
 function render() {
